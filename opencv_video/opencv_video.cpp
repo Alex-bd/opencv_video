e801fcf1b35b4fadea160a6848273ca01da95a99
bd w <1178276956@qq.com>
2019-10-01 16:29:44 +0800

    通过OpenCV读取视频

--- opencv_video/opencv_video.cpp
+++ opencv_video/opencv_video.cpp
@@ -3,10 +3,39 @@
 
 #include "pch.h"
 #include <iostream>
-
+#include<opencv2/core.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+using namespace cv;
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	std::cout <<"读取千峰教育python视频";
+	VideoCapture capure("D:1.MP4");		//创建视频对象
+	if(!capure.isOpened())		//判断是否正常打开视频
+	{
+		return 1;
+	}
+	double rate = capure.get(CV_CAP_PROP_FPS);	//获取帧率
+	bool stop(false);
+	Mat frame;	//当前视频帧
+	namedWindow("Extracted Frame");
+	
+	int delay = 1000 / rate;	
+	while (!stop)		//如果不到视频最后，就一直循环帧
+	{
+		if (!capure.read(frame))	//把capure的视频帧读到frame图片上
+		{
+			break;
+		}
+		//resizeWindow("Extracted Frame", 800, 600);
+		imshow("Extracted Frame", frame);	//在窗口中显示出来
+		if (waitKey(delay)>= 0)//允许通过任意键来中断读取
+			//如果延迟参数设置为0，则会无限等待用户按键
+		{
+			stop = true;
+		}
+	}
+	capure.release();
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
