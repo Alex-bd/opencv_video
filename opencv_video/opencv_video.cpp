8e499cafed9b355ef95153bf4ac0e7771ef9397a
bd w <1178276956@qq.com>
2019-10-23 23:58:27 +0800

    canny视频边缘检测

--- opencv_video/opencv_video.cpp
+++ opencv_video/opencv_video.cpp
@@ -9,42 +9,26 @@
 using namespace cv;
 int main()
 {
-	std::cout <<"读取千峰教育python视频";
-	VideoCapture capure("D:1.MP4");		//创建视频对象
-	if(!capure.isOpened())		//判断是否正常打开视频
-	{
-		return 1;
-	}
-	double rate = capure.get(CV_CAP_PROP_FPS);	//获取帧率
+	VideoCapture capture("D:/movie/surface.mp4");	//捕获视频
+	if (!capture.isOpened())
+		return 1;	
+	double rate = capture.get(CV_CAP_PROP_FPS);	//获取视频参数	
 	bool stop(false);
-	Mat frame;	//当前视频帧
-	namedWindow("Extracted Frame");
+	Mat frame;		//声明图像格式变量
+	namedWindow("Canny Video");		
+	int delay = 100 / rate;
 	
-	int delay = 1000 / rate;	
-	while (!stop)		//如果不到视频最后，就一直循环帧
+	while (!stop)
 	{
-		if (!capure.read(frame))	//把capure的视频帧读到frame图片上
-		{
-			break;
-		}
-		//resizeWindow("Extracted Frame", 800, 600);
-		imshow("Extracted Frame", frame);	//在窗口中显示出来
-		if (waitKey(delay)>= 0)//允许通过任意键来中断读取
-			//如果延迟参数设置为0，则会无限等待用户按键
-		{
-			stop = true;
-		}
+	if (!capture.read(frame))
+		break;
+		Mat result;		
+		Canny(frame, result, 100, 180);	//输入，输出，阈值1，阈值2
+		threshold(result, result, 128, 255, THRESH_BINARY);
+		imshow("Canny Video", result);
+
+		if (waitKey(delay) >= 0)
+		stop = true;
 	}
-	capure.release();
+	capture.release();
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
